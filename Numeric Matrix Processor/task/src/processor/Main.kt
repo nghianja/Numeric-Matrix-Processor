cbbb6e46e8cdc3639fad875c6e475e8f4f7c3392
James Ng <james.ng@projapps.com>
2020-03-22 02:20:49 +0800

    Complete Stage #1

--- Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric Matrix Processor/task/src/processor/Main.kt
@@ -1,5 +1,29 @@
 package processor
 
 fun main() {
-    println("Hello World!")
+    val (nA, mA) = readLine()!!.split(" ")
+    val xA = getMatrix(nA, mA)
+    val (nB, mB) = readLine()!!.split(" ")
+    val xB = getMatrix(nB, mB)
+    if (nA.toInt() != nB.toInt() || mA.toInt() != mB.toInt()) {
+        println("ERROR")
+    } else {
+        for (i in 1..nA.toInt()) {
+            for (j in 1..mA.toInt()) {
+                print("${xA[i - 1][j - 1] + xB[i - 1][j - 1]} ")
+            }
+            println()
+        }
+    }
+}
+
+private fun getMatrix(n: String, m: String): Array<IntArray> {
+    val x = Array(n.toInt()) { IntArray(m.toInt()) { 0 } }
+    for (i in 1..n.toInt()) {
+        val cols = readLine()!!.split(" ")
+        for (j in 1..m.toInt()) {
+            x[i - 1][j - 1] = cols[j - 1].toInt()
+        }
+    }
+    return x
 }
