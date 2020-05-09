21f840267d739572882d724c1ff5c1297cbac50c
James Hian Ng <james.ng@projapps.com>
2020-05-10 02:54:46 +0800

    Completed Python Stage #2

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,18 +1,21 @@
-def getMatrix(dimen):
+def get_matrix(dimen):
     matrix = []
     for i in range(0, dimen[0]):
         matrix.append([int(i) for i in input().split()])
     return matrix
 
 
+def add_matrices(xA, xB):
+    matrix = []
+    for i in range(0, len(xA)):
+        matrix.append([xA[i][j] + xB[i][j] for j in range(0, len(xA[0]))])
+    return matrix
+
+
 if __name__ == '__main__':
     dA = tuple([int(i) for i in input().split()])
-    xA = getMatrix(dA)
-    dB = tuple([int(i) for i in input().split()])
-    xB = getMatrix(dB)
-    if dA[0] != dB[0] or dB[1] != dB[1]:
-        print("ERROR")
-    else:
-        for i in range(0, dA[0]):
-            row = [xA[i][j] + xB[i][j] for j in range(0, dA[1])]
-            print(*row)
+    xA = get_matrix(dA)
+    c = int(input())
+    for i in range(0, dA[0]):
+        row = [c * xA[i][j] for j in range(0, dA[1])]
+        print(*row)
