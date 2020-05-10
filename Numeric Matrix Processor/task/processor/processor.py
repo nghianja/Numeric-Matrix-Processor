e9f1fcef4cab2dad975431b2eb14c6631881e681
James Hian Ng <james.ng@projapps.com>
2020-05-10 22:33:21 +0800

    Completed Python Stage #4

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -46,11 +46,73 @@ def multiply_matrices():
             print(*r)
 
 
+def transpose_matrix():
+    print()
+    print("1. Main diagonal")
+    print("2. Side diagonal")
+    print("3. Vertical line")
+    print("4. Horizontal line")
+    choice = int(input("Your choice: "))
+    if choice == 1:
+        main_diagonal()
+    elif choice == 2:
+        side_diagonal()
+    elif choice == 3:
+        vertical_line()
+    elif choice == 4:
+        horizontal_line()
+
+
+def main_diagonal():
+    x = get_matrix(get_size("Enter size of matrix: "))
+    print("The transpose result is:")
+    t = [[0.0] * len(x[0]) for _n in range(len(x))]
+    for i in range(len(x)):
+        for j in range(len(x[0])):
+            t[j][i] = x[i][j]
+    for r in t:
+        print(*r)
+
+
+def side_diagonal():
+    x = get_matrix(get_size("Enter size of matrix: "))
+    print("The transpose result is:")
+    t = [[0.0] * len(x[0]) for _n in range(len(x))]
+    for i in range(len(x)):
+        for j in range(len(x[0])):
+            t[i][j] = x[len(x[0]) - 1 - j][len(x) - 1 - i]
+    for r in t:
+        print(*r)
+
+
+def vertical_line():
+    x = get_matrix(get_size("Enter size of matrix: "))
+    print("The transpose result is:")
+    t = [[0.0] * len(x[0]) for _n in range(len(x))]
+    for i in range(len(x)):
+        for j in range(len(x[0])):
+            t[i][j] = x[i][len(x[0]) - 1 - j]
+    for r in t:
+        print(*r)
+
+
+def horizontal_line():
+    x = get_matrix(get_size("Enter size of matrix: "))
+    print("The transpose result is:")
+    t = [[0.0] * len(x[0]) for _n in range(len(x))]
+    for i in range(len(x)):
+        for j in range(len(x[0])):
+            t[i][j] = x[len(x) - 1 - i][j]
+    for r in t:
+        print(*r)
+
+
 if __name__ == '__main__':
     while True:
         print("1. Add matrices")
         print("2. Multiply matrix to a constant")
         print("3. Multiply matrices")
+        print("4. Transpose matrix")
         print("0. Exit")
         choice = int(input("Your choice: "))
         if choice == 1:
@@ -59,5 +121,7 @@ if __name__ == '__main__':
             multiply_matrix_to_constant()
         elif choice == 3:
             multiply_matrices()
+        elif choice == 4:
+            transpose_matrix()
         elif choice == 0:
             break
